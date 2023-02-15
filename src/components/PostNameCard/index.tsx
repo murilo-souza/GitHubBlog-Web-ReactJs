a39bfffd2863467e5aa334a0fe6eb53ccc4056cf
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-02-15 19:11:26 -0300

    update

--- src/components/PostNameCard/index.tsx
+++ src/components/PostNameCard/index.tsx
@@ -8,38 +8,17 @@ import {
   ChatCircle,
   GithubLogo,
 } from 'phosphor-react'
-<<<<<<< HEAD
 
 interface PostDetail {
   title: string
-=======
-import { formatDistanceToNow } from 'date-fns'
-import { ptBR } from 'date-fns/locale'
 
-interface Props {
-  title: string
-  created_at: Date
->>>>>>> 576c3a77ff4adaf28a06cd0bae280b1710f339d2
   comments: number
   html_url: string
   user: {
     login: string
   }
-<<<<<<< HEAD
   created_at: string
 }
-=======
-}
-
-export function PostNameCard({
-  title,
-  created_at,
-  comments,
-  html_url,
-  user,
-}: Props) {
-  const date = new Date(created_at)
->>>>>>> 576c3a77ff4adaf28a06cd0bae280b1710f339d2
 
 export function PostNameCard({
   title,
@@ -61,11 +40,7 @@ export function PostNameCard({
       <Title>{title}</Title>
       <InfoArea>
         <Info title={user.login} icon={GithubLogo} />
-<<<<<<< HEAD
         <Info title={created_at} icon={Calendar} />
-=======
-        <Info title={publishedDateRelativeToNow} icon={Calendar} />
->>>>>>> 576c3a77ff4adaf28a06cd0bae280b1710f339d2
         <Info title={`${comments} comentários`} icon={ChatCircle} />
       </InfoArea>
     </Container>
